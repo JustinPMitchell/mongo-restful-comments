21804145e0701a79ab76b936471f87526d6ae4d2
JustinPMitchell <j.mitchell.real@gmail.com>
2018-01-27 20:38:04 -0800

    CommentList component stubbed out to take in comment

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import CommentList from './CommentList.js';
-import CommentForm from './CommentForm.js';
-import DATA from './data';
-import style from './style';
+// import CommentForm from './CommentForm.js';
+// import DATA from './data.js';
+import style from './style.js';
 
 class CommentBox extends Component {
   constructor(props) {
-    super(props) {
-      this.state = { data: [] };
-    }
+    super(props);
+    this.state = { data: [] };
   }
   render() {
-    <div style={ style.commentBox }>
-      <h2>Comments:</h2>
-      <CommentList data={ DATA }/>
-      <CommentForm />
-    </div>
+    return (
+      <div style={ style.commentBox }>
+        <h2>Comments:</h2>
+        <CommentList /*data={ DATA }*/ />
+{/*        <CommentForm />*/}
+      </div>
+    );
   }
 }
 
