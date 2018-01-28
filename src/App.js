4269690f2cc2f47fb4d451869a6081bfeb25890e
JustinPMitchell <j.mitchell.real@gmail.com>
2018-01-27 20:13:15 -0800

    stubbed out commentBox to take in commentList and CommentForm components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className='title'>Team Tustin</h1>
+        <h1>Team Tustin</h1>
       </div>
     );
   }
